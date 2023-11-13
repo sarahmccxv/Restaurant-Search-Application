e4a806412049e9786adb7722bd4d02157e1f193e
Huiyang Chen <huiyang.chen@mail.utoronto.ca>
2023-11-13 03:51:10 -0500

    Started working on WriteReview interactor

---
+++ src/use_case/write_review/WriteReviewInteractor.java
@@ -0,0 +1,11 @@
+package use_case.write_review;
+
+public class WriteReviewInteractor implements WriteReviewInputBoundary {
+    public WriteReviewInteractor() {
+
+    }
+    @Override
+    public void execute(WriteReviewInputData writeReviewInputData) {
+
+    }
+}
