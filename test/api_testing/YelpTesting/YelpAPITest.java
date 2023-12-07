f588590f14d737f0e800ebc830db120e3e144d59
Huiyang Chen <huiyang.chen@mail.utoronto.ca>
2023-12-06 23:06:21 -0500

    # Conflicts: #	test/api_testing/YelpTesting/YelpAPITest.java

--- test/api_testing/YelpTesting/YelpAPITest.java
+++ test/api_testing/YelpTesting/YelpAPITest.java
@@ -93,6 +93,8 @@ public class YelpAPITest {
         when(yelpApiServices.getReviews(reviewCriteria)).thenReturn(reviews); // stub the getReviews method to return the fake list
 
         assertEquals(expectedRestaurantID, reviews.get(0).getRestaurantID());
+        assertEquals("Lina H.", reviews.get(0).getAuthor().getUsername());
+        assertEquals("2023-12-04T08:20:46", reviews.get(0).getCreationTime().toString());
         assertEquals("Euan S.", reviews.get(0).getAuthor().getUsername());
         assertEquals(creationTime.toString(), reviews.get(0).getCreationTime().toString());
     }
